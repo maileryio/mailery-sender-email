147a6357323ec39c9915f98a018803f61510787b
Igor Maliy <imaliy.filsh@gmail.com>
2021-03-27 17:04:29 +0200

    Development

--- src/Provider/RouteCollectorServiceProvider.php
+++ src/Provider/RouteCollectorServiceProvider.php
@@ -21,22 +21,25 @@ final class RouteCollectorServiceProvider extends ServiceProvider
         $collector = $container->get(RouteCollectorInterface::class);
 
         $collector->addGroup(
-            Group::create(
-                '/brand/{brandId:\d+}',
-                [
+            Group::create('/brand/{brandId:\d+}')
+                ->routes(
                     // Senders:
-                    Route::get('/sender/emails', [DefaultController::class, 'index'])
-                        ->name('/sender/email/index'),
-                    Route::get('/sender/email/view/{id:\d+}', [DefaultController::class, 'view'])
-                        ->name('/sender/email/view'),
-                    Route::methods(['GET', 'POST'], '/sender/email/create', [DefaultController::class, 'create'])
-                        ->name('/sender/email/create'),
-                    Route::methods(['GET', 'POST'], '/sender/email/edit/{id:\d+}', [DefaultController::class, 'edit'])
-                        ->name('/sender/email/edit'),
+                    Route::get('/sender/emails')
+                        ->name('/sender/email/index')
+                        ->action([DefaultController::class, 'index']),
+                    Route::get('/sender/email/view/{id:\d+}')
+                        ->name('/sender/email/view')
+                        ->action([DefaultController::class, 'view']),
+                    Route::methods(['GET', 'POST'], '/sender/email/create')
+                        ->name('/sender/email/create')
+                        ->action([DefaultController::class, 'create']),
+                    Route::methods(['GET', 'POST'], '/sender/email/edit/{id:\d+}')
+                        ->name('/sender/email/edit')
+                        ->action([DefaultController::class, 'edit']),
 
-                    Route::get('/sender/email/verify/{id:\d+}/{token:\w+}', [DefaultController::class, 'verify'])
-                        ->name('/sender/email/verify'),
-                ]
+                    Route::get('/sender/email/verify/{id:\d+}/{token:\w+}')
+                        ->name('/sender/email/verify')
+                        ->action([DefaultController::class, 'verify'])
             )
         );
     }
