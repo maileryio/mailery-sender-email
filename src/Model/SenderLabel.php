44de2c01a08a08b1f5b79e855b506c02a67d714e
Igor Maliy <imaliy.filsh@gmail.com>
2021-04-05 18:09:29 +0300

    Development

---
+++ src/Model/SenderLabel.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace Mailery\Sender\Email\Model;
+
+class SenderLabel
+{
+    /**
+     * @var string|null
+     */
+    private ?string $name = null;
+
+    /**
+     * @var string|null
+     */
+    private ?string $email = null;
+
+    /**
+     * @param string|null $name
+     * @param string|null $email
+     */
+    public function __construct(string $name = null, string $email = null)
+    {
+        $this->name = $name;
+        $this->email = $email;
+    }
+
+    /**
+     * @return string
+     */
+    public function __toString()
+    {
+        if (empty($this->name)) {
+            return $this->email;
+        }
+
+        return sprintf('%s <%s>', $this->name, $this->email);
+    }
+
+    /**
+     * @param string $string
+     * @return self[]
+     */
+    public static function fromString(string $string): array
+    {
+        $emails = [];
+
+        if(preg_match_all('/\s*"?([^><,"]+)"?\s*((?:<[^><,]+>)?)\s*/', $string, $matches, PREG_SET_ORDER) > 0) {
+            foreach($matches as $m) {
+                if(!empty($m[2])) {
+                    $emails[trim($m[2], '<>')] = $m[1];
+                } else {
+                    $emails[$m[1]] = '';
+                }
+            }
+        }
+
+        $results = [];
+
+        foreach ($emails as $email => $name) {
+            $results[] = new self($name, $email);
+        }
+
+        return $results;
+    }
+
+    /**
+     * @return string
+     */
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    /**
+     * @return string
+     */
+    public function getEmail(): string
+    {
+        return $this->email;
+    }
+}
