534253c191816ad904ac69a39b0247879acec030
Igor Maliy <imaliy.filsh@gmail.com>
2022-06-20 01:14:39 +0300

    Development

--- views/default/_layout.php
+++ views/default/_layout.php
@@ -21,7 +21,7 @@ $this->setTitle($sender->getName());
                     <div class="col-md">
                         <h4 class="mb-0">Sender #<?= $sender->getId(); ?></h4>
                         <p class="mt-1 mb-0 small">
-                            Changed at <?= DateTimeFormat::widget()->dateTime($sender->getUpdatedAt())->run() ?>
+                            Changed at <?= DateTimeFormat::widget()->dateTime($sender->getUpdatedAt()) ?>
                         </p>
                     </div>
                     <div class="col-auto">
